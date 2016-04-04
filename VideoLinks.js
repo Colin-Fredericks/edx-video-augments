5fafb5fe0074ecb50d87a4904f6f50770e347037
Colin-Fredericks <colin.fredericks@gmail.com>
2016-04-04 10:40:16 -0400

    Clear out old loops when they're not needed.

--- VideoLinks.js
+++ VideoLinks.js
@@ -185,7 +185,14 @@ $(document).ready(function(){
         
         var timeChecker = setInterval(function(){
             
-            state.videoPlayer.update();        // Forced update of time. Required for Safari.
+            try{
+                state.videoPlayer.update();        // Forced update of time. Required for Safari.
+            }
+            catch(err){
+                // If this fails, shut down this loop.
+                // it's probably because we moved to a new tab.
+                clearInterval(timeChecker);
+            }
             time = state.videoPlayer.currentTime;
             
             // If we should be showing a link:
